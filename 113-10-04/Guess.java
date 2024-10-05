e9de0a3ac21d007ef27cbc581eeadd8797a8033b
soy0424zxc <A111222011@mail.shu.edu.tw>
2024-10-06 01:27:07 +0800

    113-10-04

---
+++ 113-10-04/Guess.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) {
+        var console = new Scanner(System.in);
+        var number = (int) (Math.random() * 10);
+        var guess = -1;
+        
+        do {
+            System.out.print("�q�Ʀr�]0 ~ 9�^:");
+            guess = console.nextInt();
+        } while(guess != number);
+        
+        System.out.println("�q���F...XD");
+    }
+}
---
+++ 113-10-04/SecureRandomDemo.java
@@ -0,0 +1,16 @@
+import java.security.SecureRandom;
+
+public class SecureRandomDemo {
+    public static void main(String[] args) {
+        SecureRandom randomNumber = new SecureRandom();
+        while(true) {
+            var number = randomNumber.nextInt(10);
+            System.out.println(number);
+            
+            if(number == 5) {
+                System.out.println("I hit 5....Orz");
+                break;
+            }
+        }
+    }
+}
